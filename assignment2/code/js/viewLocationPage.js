9ba41b5c2c868609395dc3579316fd89fcc800b3
xzho172 <xzho172@student.monash.edu>
2016-05-17 14:03:38 +1000

    Location Weather Summary
    Not complete

--- assignment2/code/js/viewLocationPage.js
+++ assignment2/code/js/viewLocationPage.js
@@ -10,3 +10,42 @@ if (locationIndex !== null)
     // If a location name was specified, use it for header bar title.
     document.getElementById("headerBarTitle").textContent = locationNames[locationIndex];
 }
+
+var APIID = "mbal38@student.monash.edu";
+var temp;
+var loc;
+var icon;
+var humidity;
+var wind;
+var direction;
+
+
+function update(weather)
+{
+    temp.innerHTML = weather.temp;
+    loc.inner = weather.loc;
+    wind.innerHTML = weather.wind;
+    direction.innerHTML = weather.direction;
+    humidity.innerHTML = weather.humidity;
+    icon.src = "imgs/code/" + weather.icon + ".png";
+    console.log(icon.src)
+}
+
+window.onload = function ()
+{
+    temp = document.getElementById("temperature");
+    loc = document.getElementById("location");
+    icon = document.getElementById("icon");
+    humidity = document.getElementById("humidity");
+    wind = document.getElementById("direction");
+    
+    var weather = {};
+    weather.temp = ;
+    weather.wind = ;
+    weather.dirction = ;
+    weather.humidity =  ;
+    weather.loc = ;
+    weather.icon = ;
+     
+    update(weather);
+}
