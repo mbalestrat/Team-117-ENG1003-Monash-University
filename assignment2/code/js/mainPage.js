cf3c95943c1e35f57de9fc61ae368a996a32c98f
kaiconut <klzho2@student.monash.edu>
2016-05-23 04:53:00 +1000

    creating MDL list for main page
    - connected onclick function to view main page
    - added ripple effect to add location button
    - removed sample code

--- assignment2/code/js/mainPage.js
+++ assignment2/code/js/mainPage.js
@@ -3,29 +3,9 @@
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
 
-//function viewLocation(locationName)
-{
-    // Save the desired location to local storage
-    //localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName); 
-    // And load the view location page.
-    //location.href = 'viewlocation.html';
+function viewLocation(locationName) {
+    Save the desired location to local storage
+    localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName);
+    And load the view location page.
+    location.href = 'viewlocation.html';
 }
-
-function viewlocation(locationName)
-{
-    //code to save location object to Local Storage
-    outputAreaRef.innerHTML = JSON.stringify(locations);
-    localStorage.setItem(APP_PREFIX,JSON.stringify(locations));
-    //oad the view location page
-    location.href = 'viewlocation.html'
-}
-
-
-
-
-
-
-
-
-
-
