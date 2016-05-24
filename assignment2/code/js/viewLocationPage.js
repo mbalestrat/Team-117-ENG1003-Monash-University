10e7620d192787e1835b921d25bab8d2b688eb6d
xzho172 <xzho172@student.monash.edu>
2016-05-24 12:21:38 +1000

    Map
    There is a map showing on the view loaction page

--- assignment2/code/js/viewLocationPage.js
+++ assignment2/code/js/viewLocationPage.js
@@ -3,17 +3,33 @@
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
 
-var APP_PREFIX="key1";
+var APP_PREFIX = "key1";
 
-var locationIndex = localStorage.getItem(APP_PREFIX + "-selectedLocation"); 
-if (locationIndex !== null)
-{
-    var locationNames = [ "Location A", "Location B" ];
+var locationIndex = localStorage.getItem(APP_PREFIX + "-selectedLocation");
+if (locationIndex !== null) {
+    var locationNames = ["Location A", "Location B"];
     // If a location name was specified, use it for header bar title.
     document.getElementById("headerBarTitle").textContent = locationNames[locationIndex];
-    
+
 }
 
+var map;
+
+function initMap() {
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        }
+    });
+
+    var geocoder = new google.maps.Geocoder();
+    fieldValueChanged = function () {
+        geocoderAddress(geocoder, map);
+    };
+}
 
 
 //Adate selection slider
