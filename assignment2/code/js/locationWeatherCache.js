b5a4536e6dec5c3c487e32f0b4fd7df0f800db25
Michelle Balestrat <mbal38@student.monash.edu>
2016-05-29 11:57:09 +1000

    Upload my offline work and documents

---
+++ assignment2/code/js/locationWeatherCache.js
@@ -0,0 +1,254 @@
+/*===============================================================================================================
+TEAM 117
+Assignment 2, ENG1003
+Last Edited: 24/05/2016
+Written by Michelle Balestrat, 23838213
+===============================================================================================================*/
+
+// Skeleton Functions:
+// Returns a date in the format "YYYY-MM-DD".
+Date.prototype.simpleDateString = function () {
+    function pad(value) {
+        return ("0" + value).slice(-2);
+    }
+
+    var dateString = this.getFullYear() + "-" +
+        pad(this.getMonth() + 1, 2) + '-' +
+        pad(this.getDate(), 2);
+
+    return dateString;
+}
+
+// Date format required by forecast.io API.
+// We always represent a date with a time of midday,
+// so our choice of day isn't susceptible to time zone errors.
+Date.prototype.forecastDateString = function () {
+    return this.simpleDateString() + "T12:00:00";
+}
+
+
+//------------------------------------------------------------------
+
+// Code for LocationWeatherCache class and other shared code.
+var currentLat;
+var currentLng;
+var locationWeatherCache;
+//
+
+// Prefix to use for Local Storage.
+var APP_PREFIX = "weatherApp";
+
+//------------------------------------------------------------------
+// MAIN FUNCTION
+
+function LocationWeatherCache() {
+    // Private attributes:
+
+    var locations = [];
+    var callbacks = {};
+    var APIkey = "760b8e8a0a5f4992d672bcb53a07998e";
+
+
+    // Public methods:
+
+    // Returns the number of locations stored in the cache.
+    //
+    this.length = function () {
+        return locations.length;
+    };
+
+    //------------------------------------------------------------------
+    // Returns the location object for a given index. USE ON VIEW LOCATION PAGE & MAIN SCREEN
+    // Indexes begin at zero.
+    //
+    this.locationAtIndex = function (index) {
+        return locations[index];
+    };
+
+    //------------------------------------------------------------------
+    // Given a latitude, longitude and nickname, this method saves a 
+    // new location into the cache.  It will have an empty 'forecasts'
+    // property.  Returns the index of the added location.
+    //
+    this.addLocation = function (latitude, longitude, nickname) {
+        // Create the newLoc object 
+        var newLoc = {
+            lat: latitude,
+            long: longitude,
+            nick: nickname,
+            forecasts: {}
+        };
+
+        // NB: Getting weather is the job of getWeatherAtIndexForDate function
+        locations.push(newLoc);
+
+        // Push the new location to the array
+        index = locations.length - 1;
+
+        //index = locations.length - (spliceCount - 1);
+        saveLocations();
+
+        // Return index of added location
+        return index
+    };
+
+    //------------------------------------------------------------------
+    // Removes the saved location at the given index.
+    // 
+    this.removeLocationAtIndex = function (index) {
+        if (index > -1) {
+            locations.splice(index, 1);
+            saveLocations();
+            location.href = 'index.html'
+        }
+    };
+
+    //------------------------------------------------------------------
+    // This method is used by JSON.stringify() to serialise this class.
+    // Note that the callbacks attribute is only meaningful while there 
+    // are active web service requests and so doesn't need to be saved.
+    //
+    this.toJSON = function () {
+        var locationWeatherPDO = {
+            locations: locations,
+            callbacks: callbacks
+        };
+
+        return locationWeatherPDO;
+    };
+
+    //------------------------------------------------------------------
+    //Sets private attributes for a class instance. Lets LocalWeather be recreated on app relaunch.
+    //this.initialiseFromJSON = function()
+    //{
+    //    
+    //};
+
+    //------------------------------------------------------------------
+    // Given a public-data-only version of the class (such as from
+    // local storage), this method will initialise the current
+    // instance to match that version.
+    //
+    this.initialiseFromPDO = function (locationWeatherPDO) {
+        //  locationWeatherCacheObj =locationWeatherPDO;// JSON.parse(locationWeatherPDO);
+        locations = locationWeatherPDO.locations;
+    };
+
+    //------------------------------------------------------------------
+    // Request weather for the location at the given index for the
+    // specified date.  'date' should be JavaScript Date instance.
+    //
+    // This method doesn't return anything, but rather calls the 
+    // callback function when the weather object is available. This
+    // might be immediately or after some indeterminate amount of time.
+    // The callback function should have two parameters.  The first
+    // will be the index of the location and the second will be the 
+    // weather object for that location.
+    // 
+    this.getWeatherAtIndexForDate = function (index, date, callback) {
+
+        var place = locations[index];
+        var key = place.lat + ',' + place.long + ',' + date;
+
+        //Check if weather data point for date is already in forecasts array.
+        if (place.forecasts.hasOwnProperty(key)) {
+
+            // If yes, call callback function with weather data.
+            return callback(index, place.forecasts[key]);
+        }
+
+        if (callbacks[key] == undefined) {
+            // If not, call forecast API with JSONP
+            callbacks[key] = [callback, index];
+            jsonpRequest("https://api.forecast.io/forecast/" + APIkey, key);
+        }
+
+    };
+
+    //------------------------------------------------------------------
+    // This is a callback function passed to forecast.io API calls.
+    // This will be called via JSONP when the API call is loaded.
+    //
+    // This should invoke the recorded callback function for that
+    // weather request.
+    //
+
+    this.weatherResponse = function (response) {
+
+        var WeatherForecast = response.daily;
+        var date = new Date(WeatherForecast.data[0].time * 1000);
+        date = date.forecastDateString();
+
+        var callbackIndex = response.latitude + ',' + response.longitude + ',' + date;
+
+        var callback = callbacks[callbackIndex][0];
+        index = callbacks[callbackIndex][1];
+        //var index = locations.length;
+
+        delete callbacks[callbackIndex];
+
+        locations[index].forecasts[callbackIndex] = WeatherForecast;
+
+        saveLocations(locations);
+        document.body.appendChild(script);
+
+        callback(index, WeatherForecast);
+    };
+
+
+    //------------------------------------------------------------------
+    // Private methods:
+
+    // Given a latitude and longitude, this method looks through all
+    // the stored locations and returns the index of the location with
+    // matching latitude and longitude if one exists, otherwise it
+    // returns -1.
+    //
+    this.getIndexByLatLng = function (lat, long) {
+        for (var i = 0; i < locations.length; i++) {
+            if (locations[i].lat === lat && locations[i].long === long) {
+                return (i);
+            }
+        }
+        return (-1);
+    }
+};
+
+function jsonpRequest(url, data) {
+    // Build URL parameters from data object.
+    var params = "";
+    params += "/" + data;
+
+    params += "/?exclude=[currently,minutely,hourly,alerts]&units=si&callback=locationWeatherCache.weatherResponse";
+
+    var script = document.createElement('script');
+    script.src = url + params;
+    document.body.appendChild(script);
+}
+
+//================================================================
+// Restore the singleton locationWeatherCache from Local Storage.
+//
+function loadLocations() {
+    // Create a single LocationWeatherCache class instance inside global variable 'LocationWeatherCache'.
+
+    var locationWeatherFromStorageJSON = localStorage.getItem(APP_PREFIX);
+    locationWeatherCache = new LocationWeatherCache();
+
+    if (!(locationWeatherFromStorageJSON === null || locationWeatherFromStorageJSON === "undefined")) {
+        locationWeatherPDO = JSON.parse(locationWeatherFromStorageJSON);
+        locationWeatherCache.initialiseFromPDO(locationWeatherPDO);
+    }
+
+}
+//================================================================
+// Save the singleton locationWeatherCache to Local Storage.
+//
+function saveLocations() {
+    localStorage.setItem(APP_PREFIX, JSON.stringify(locationWeatherCache));
+}
+
+//================================================================
+// Call loadLocations function so that stored data is available on all pages.
+
+loadLocations();
---
+++ assignment2/code/js/viewLocationPage.js
@@ -0,0 +1,160 @@
+// Code for the View Location page.
+
+// This is sample code to demonstrate navigation.
+// You need not use it for final app.
+
+var outputAreaRef = document.getElementById("locDate");
+var output = "";
+var currentDate = new Date();
+var dateStr = currentDate.simpleDateString();
+var forecastDate = currentDate.forecastDateString();
+
+outputAreaRef.textContent = dateStr;
+
+//-----------------------------------------------------------------------------
+
+var APP_PREFIX = "weatherApp";
+var weatherData = [];
+
+var chosenLocation = localStorage.getItem(APP_PREFIX + "-selected");
+var chosenLocationObj = JSON.parse(chosenLocation);
+
+//Pull data from localStorage capture
+
+//Extract name of location
+var locRawName = JSON.stringify(chosenLocationObj.nick);
+var loc = locRawName.split('"').join('');
+
+//Extract co-ordinates for map
+var latRaw = JSON.stringify(chosenLocationObj.lat);
+var latitude = Number(latRaw.split('"').join(''));
+
+var longRaw = JSON.stringify(chosenLocationObj.long);
+var longitude = Number(longRaw.split('"').join(''));
+
+//Get the index from the lat/lng
+index = locationWeatherCache.getIndexByLatLng(latitude, longitude);
+
+locationWeatherCache.getWeatherAtIndexForDate(index, forecastDate, viewPageWeatherResponse);
+
+// The part below could be changed by the slider movement.
+function viewPageWeatherResponse(index, response) // the weather obj
+{
+
+    var locRaw = locationWeatherCache.locationAtIndex(index);
+
+    //Extract Weather from location
+    var key = locRaw.lat + ',' + locRaw.long + ',' + forecastDate;
+    var weatherInfoRaw = locRaw.forecasts[key];
+
+    //Insert call to API here
+    //Perhaps use if/else statement if forecastDate == the current forecast
+
+    //Get the summary
+    var weatherInfo = JSON.stringify(weatherInfoRaw.data[0].summary);
+    var summary = weatherInfo.split('"').join('');
+
+    //Get the Lo Temps
+    var loTemp = JSON.stringify(weatherInfoRaw.data[0].apparentTemperatureMin);
+    var loCel = loTemp.split('"').join('');
+
+    //Get the Hi Temps
+    var hiTemp = JSON.stringify(weatherInfoRaw.data[0].apparentTemperatureMax);
+    var hiCel = hiTemp.split('"').join('');
+
+    //Get the Chance of Rain
+    var precip = JSON.stringify(weatherInfoRaw.data[0].precipProbability);
+    var prob = precip.split('"').join('');
+
+    //Get the wind speed
+    var wind = JSON.stringify(weatherInfoRaw.data[0].windSpeed);
+    var windSp = precip.split('"').join('');
+
+    //Get the barometric pressure
+    var pressure = JSON.stringify(weatherInfoRaw.data[0].pressure);
+    var baro = pressure.split('"').join('');
+
+    var humidity = JSON.stringify(weatherInfoRaw.data[0].humidity);
+    var humid = humidity.split('"').join('');
+
+
+    //Output to page
+    document.getElementById("summary").innerHTML = "<strong>" + summary + "</strong>" + "<br> Chance of Rain: " + precip + "%" + "<br> Max/Min: " + hiCel + "&deg;C / " + loCel + "&deg;C" + "<br> Wind Speed: " + windSp + "km/h" + "<br> Humidity: " + humid + "%" + "<br> Barometric Pressure: " + baro + " atm";
+}
+
+
+//Header Bar
+if (chosenLocation !== null) {
+    document.getElementById("headerBarTitle").textContent = loc;
+} else {
+    var locationNames = ["Location A", "Location B"];
+    // If a location name was specified, use it for header bar title.
+    document.getElementById("headerBarTitle").textContent = locationNames[locationIndex];
+}
+
+//The date that dispalyedd weather applies to
+// Returns a date in the format "YYYY-MM-DD".
+
+Date.prototype.simpleDateString = function () {
+    function pad(value) {
+        return ("0" + value).slice(-2);
+    }
+
+    var dateString = this.getFullYear() + "-" +
+        pad(this.getMonth() + 1, 2) + '-' +
+        pad(this.getDate(), 2);
+
+    return dateString;
+}
+
+
+//map displays on the page
+var map;
+
+function locMap() {
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: {
+            lat: latitude,
+            lng: longitude
+        }
+    });
+
+
+    var geocoder = new google.maps.Geocoder();
+    fieldValueChanged = function () {
+        geocoderAddress(geocoder, map);
+    };
+
+    var marker = new google.maps.Marker({
+        position: {
+            lat: latitude,
+            lng: longitude
+        },
+        map: map,
+        title: loc
+    });
+}
+
+// Slider calls the getWeatherAtIndexForDate function, with a different callback function to display results on this page.
+
+
+//A date selection slider
+//30 positions
+//set to the far right
+var slide = document.getElementById('slide');
+var forecastDate;
+
+function slideChange() {
+    //Each position means - 24 hours
+    var slideDate = new Date();
+    slideDate.setDate(slideDate.getDate() - (-1 * slide.value));
+
+    //Get weather for location at new date
+    //chosenLocationObj = locationWeatherCache.getWeatherAtIndexForDate();
+
+    locDate.innerHTML = slideDate.simpleDateString();
+    forecastDate = slideDate.forecastDateString();
+    locationWeatherCache.getWeatherAtIndexForDate(index, forecastDate, viewPageWeatherResponse);
+}
